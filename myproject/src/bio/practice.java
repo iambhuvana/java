90aeb4e9d8ec4731974c3380b81908c9606e0e2f
iambhuvana <bhuvanab120@gmail.com>
2024-05-11 20:54:35 +0530

    Bio changes and removed unwanted file
    Bio changes and removed unwanted file

---
+++ myproject/src/bio/practice.java
@@ -0,0 +1,31 @@
+package bio;
+
+public class practice {
+	public static void main(String args[]){
+		String Name= ("bhuvaneshwari");
+		long Mobile= 6382098481l ;
+		String Gender=("female");
+		String Qualification=("b.com CA");
+		long year=2020;
+		int x=70;
+		boolean completed=true;
+		boolean not_completed=false;
+		System.out.println("a.Name=" +Name);
+		System.out.println("b.Mobile Number=" +Mobile);
+		System.out.println("c.Gender=" +Gender);
+		System.out.println("d.Qualification="+Qualification);
+		System.out.println("e.year of passed out="+year);
+		System.out.println("f.percentage=" +x);
+		System.out.println("g.completed degree or not(true or false)="+completed);
+		
+		
+	}
+	
+	}
+	
+	
+	
+	
+
+
+
